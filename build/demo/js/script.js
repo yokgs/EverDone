7a9531cde2f19ab2990aac1fb294b89d4dd85caa
Yazid Slila <70753201+yokgs@users.noreply.github.com>
2020-11-22 14:13:21 +0100

    Create script.js

---
+++ build/demo/js/script.js
@@ -0,0 +1,16 @@
+var id,
+newId=function(){
+  var id='';
+  for(let i=0;i<8;i++){
+    id+=Math.floor(Math.random()*16).toString(16);
+  }
+  return id;
+};
+try{
+var id=Lockr.get('ed-id');
+}catch(a){ }
+finally{
+  id=id||newId();
+}
+alert(id);
+Lockr.get('ed-id',id);
